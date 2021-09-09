e08c0c60a3f21fa1c733cfd57bd964daebbdcc34
Metzner <jeffmetzner@rocketmortgage.com>
2021-09-09 17:08:01 -0400

    Lab 9_2

---
+++ MVCLab9_2/Controllers/Players.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCLab9_2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCLab9_2.Controllers
+{
+    public class Players : Controller
+    {
+        //public IActionResult Index()
+        //{
+         //   return View();
+        //}
+        public IActionResult Teams()
+        {
+            ViewData["FirstName"] = "Lebron";
+            ViewData["LastName"] = "James";
+            return View();
+        }
+        
+        public IActionResult AddPlayer()
+        {
+            ViewData["ErrorMessage"] = "";
+            return View();
+        }
+        public IActionResult Success(Player baller)
+        {
+
+            
+            
+            if (baller.FirstName == null || baller.LastName == null || baller.Age == null || baller.Height == null || baller.Weight == null)
+            {
+                ViewData["ErrorMessage"] = "We need all the data!  Please fill in the fields completely!";
+                return View("AddPlayer");
+            }
+            else if(baller.Age < 19 || baller.Age > 40)
+            {
+                ViewData["ErrorMessage"] = "This player does not meet the Age requirements for the NBA!";
+                return View("AddPlayer");
+            }
+            else if(baller.Height < 68 || baller.Height > 96)
+            {
+                ViewData["ErrorMessage"] = "This player does meet the Height requirements for the NBA!";
+                return View("AddPlayer");
+            }
+            else if(baller.Weight <140 || baller.Weight > 400)
+            {
+                ViewData["ErrorMessage"] = "This players does not meet the Weight requirements for the NBA!";
+                return View("AddPlayer");
+            }
+            else
+            {
+                return View(baller);
+            }
+            
+        }
+    }
+}
---
+++ MVCLab9_2/Models/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCLab9_2.Models
+{
+    public class Player
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int? Age { get; set; }
+        public int? Height { get; set; }
+        public int? Weight { get; set; }
+    }
+}
